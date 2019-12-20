ee1dd696f90d1868686d2f69c9a39bb825ce7c4c
NahuelKliphan <49241891+NahuelKliphan@users.noreply.github.com>
2019-12-20 18:53:48 -0300

    Lista de productos lista

--- src/app/component/producto/lista-producto/lista-producto.component.ts
+++ src/app/component/producto/lista-producto/lista-producto.component.ts
@@ -12,24 +12,32 @@ export class ListaProductoComponent implements OnInit {
 
     this.listadoProducto.push(this.producto1);
     this.listadoProducto.push(this.producto2);
-    //this.listadoProducto.push(this.producto3);
-    //this.listadoProducto.push(this.producto4);
+    this.listadoProducto.push(this.producto3);
+    this.listadoProducto.push(this.producto4);
+    this.listadoProducto.push(this.producto1);
+    this.listadoProducto.push(this.producto2);
+    this.listadoProducto.push(this.producto3);
+    this.listadoProducto.push(this.producto4);
 
 
-   }
+  }
 
   ngOnInit() {
   }
 
   busqueda: string = "";
 
-  producto1: Producto = new Producto('1234',399,50,'Taper');
-  producto2: Producto = new Producto('1235',879,15,'Cubiertos');
-  producto3: Producto = new Producto('1236',1500,5,'Taper');
-  producto4: Producto = new Producto('1237',87,60,'Taper');
+  producto1: Producto = new Producto('1234', 399, 50, 'Taper');
+  producto2: Producto = new Producto('1235', 879, 15, 'Cubiertos');
+  producto3: Producto = new Producto('1236', 1500, 5, 'Taper');
+  producto4: Producto = new Producto('1237', 87, 60, 'Taper rojo con bandejas de plata y oro por el costado');
 
   listadoProducto: Producto[] = [];
 
-  
+  abrirModal() {
+    
+  }
+
+
 
 }
