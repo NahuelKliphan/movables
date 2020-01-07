9a93333d18067ff63b55aa977b5b305f4671d4ed
NahuelKliphan <49241891+NahuelKliphan@users.noreply.github.com>
2020-01-07 19:31:57 -0300

    Intento de funcinar el enter en confirmar borrar producto

--- src/app/component/producto/form-borrar-producto/form-borrar-producto.component.ts
+++ src/app/component/producto/form-borrar-producto/form-borrar-producto.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Directive } from '@angular/core';
 import { BaseService } from 'src/app/servicio/base.service';
 import { Producto } from 'src/app/model/Producto';
 
-declare var alertify:any;
+declare var $: any;
 
 @Component({
   selector: 'app-form-borrar-producto',
   templateUrl: './form-borrar-producto.component.html',
   styleUrls: ['./form-borrar-producto.component.css']
 })
+
 export class FormBorrarProductoComponent implements OnInit {
 
   constructor(private base: BaseService) { }
@@ -17,8 +18,15 @@ export class FormBorrarProductoComponent implements OnInit {
     
   }
 
+  @HostListener('keydown.enter')
   borrar(){
     this.base.borrarProducto(this.base.unProducto);
+    this.vaciarCampos();
+  }
+
+  @HostListener('keydown.escape')
+  cancelar(){
+    $('#formBorrarProducto').modal('hide');
     this.vaciarCampos();
   }
 
