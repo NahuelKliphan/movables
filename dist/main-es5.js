4bb72a2e53eecf4172d9faa56c7f8a2e8e08319c
NahuelKliphan <nahuelklipanvalle@gmail.com>
2020-01-20 11:36:05 -0300

    v1.0.7 prueba

--- dist/main-es5.js
+++ dist/main-es5.js
@@ -251,7 +251,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     /* harmony default export */
 
 
-    __webpack_exports__["default"] = "v1.0.6";
+    __webpack_exports__["default"] = "v1.0.7";
     /***/
   },
 
