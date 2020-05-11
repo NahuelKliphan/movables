545319a80ac6c1578b2d3602ae22c17e08c79160
NahuelKliphan <49241891+NahuelKliphan@users.noreply.github.com>
2020-05-11 16:18:24 -0300

    Arreglo script hacer backup

--- base/CrearBackup.bat
+++ base/CrearBackup.bat
@@ -1,15 +1,16 @@
 @echo off
-for /f "tokens=1-3 delims=/ " %%i in ("%date%") do (
-    set day=%%i
-  set month=%%j
-  set year=%%k
- )
-set datestr=%day%_%month%_%year%
-echo datestr is %datestr%
-set BACKUP_FILE=D:/copia_%datestr%.backup  
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+set "fullstamp=%YYYY%-%MM%-%DD%"
+echo el nombre es copia_%fullstamp%.backup
+set BACKUP_FILE=D:/Copias/copia_%fullstamp%.backup 
 SET PGPASSWORD=postgres
 echo on
 
 pg_dump -h localhost -p 5432 -U postgres -F c stock > %BACKUP_FILE% 
 
 SET PGPASSWORD= postgres
+
+pause
+
